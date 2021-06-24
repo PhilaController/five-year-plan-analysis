d955ff6c339cceadf6eb0c18a5018f5c69f351d4
Nick Hand <nicholas.adam.hand@gmail.com>
2021-06-24 15:57:59 -0400

    update src setup to include package data

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,10 @@
 """Install the fyp_analysis package."""
 
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+here = Path(__file__).parent.absolute()
 
 entry_point = "fyp-analysis = fyp_analysis.__main__:main"
 
@@ -10,4 +14,10 @@ setup(
     version="0.1.0",
     packages=find_packages(exclude=["tests"]),
     entry_points={"console_scripts": [entry_point]},
+    package_data={
+        "fyp_analysis": [
+            "fyp_analysis/pipelines/data_processing/indicators/sources/*json"
+        ]
+    },
+    include_package_data=True,
 )
